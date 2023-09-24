578516cc911592ed7f03e63f8cc322a88828f8e2
exec zsh <kaishu0520@gmail.com>
2023-09-24 12:18:04 +0900

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App';
 import Header from './components/Header';
+import ProjectPage from './components/ProjectPage';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
   <div className="App">
     <Header />
+
+    <ProjectPage />
+
+    <AboutPage />
+
     </div>
     );
   }
