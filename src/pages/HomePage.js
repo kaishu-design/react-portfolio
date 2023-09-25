17dab537d0a75b5667bef53ce7c131002ba6e161
exec zsh <kaishu0520@gmail.com>
2023-09-25 23:52:15 +0900

    revived

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,15 +9,18 @@ const HomePage = () => {
         <div className="container">
           <div className="frofile d-flex justify-content-around">
             <div className="my-name">
-              <h2>Hello! I AM KAISHU KAWANO</h2>
-              <h4 className="mt-3">Software developer</h4>
+              <h2 className="text-center">Hello! I AM KAISHU KAWANO</h2>
+              <h4 className="text-center mt-3">Software developer</h4>
             </div>
             <div className="my-photo">
               <img src="../images/myphoto.jpg" alt="Kai" className="my-photo" />
             </div>
           </div>
-          <div className="btn btn-outline-secondary see-more">
-            <Link to="AboutPage" className="text-decoration-none see-more-text">more about me</Link>
+          <div className="skills">
+            <p>HTML, CSS, Javascript, React, Ruby on Rails, SQL, Figma, Github, Project management</p>
+            <div className="btn btn-outline-secondary see-more">
+              <Link to="AboutPage" className="text-decoration-none see-more-text">more about me</Link>
+            </div>
           </div>
         </div>
       </div>
