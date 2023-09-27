eafad9e378f753d3c798d8eda5ae40167c37a46d
exec zsh <kaishu0520@gmail.com>
2023-09-27 22:37:40 +0900

    data and iterating datas

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,38 @@
 import React from 'react'
 
+const myProjects = [
+  {
+    id:  1,
+    name: "Photographer web page",
+    description: "web page"
+  },
+  {
+    id:  2,
+    name: "LE'MAKE",
+    description: "Recipe app"
+  },
+  {
+    id:  3,
+    name: "Rent a chef",
+    description: "Rent a chef"
+  }
+]
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='container-projects'>
+      <div className='head-title'>
+        <h1>Projects</h1>
+      </div>
+      <div>
+        {myProjects.map(project => (
+          <div key={project.id}>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
