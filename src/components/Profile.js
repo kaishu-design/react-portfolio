937d7f3e2fd1a1a535f8aad48eaf3effc93b81fa
exec zsh <kaishu0520@gmail.com>
2023-10-05 00:48:08 +0900

    style up profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,11 @@ import React from 'react'
 import myPhoto from '../images/myphoto.jpg';
 import SkillList from './SkillList.js';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+
+const wantedly = "https://theme.zdassets.com/theme_assets/650189/6d25a979173b46b210b08e3e42289b1bf3e12bdc.ico"
 
 const prodfile = () => {
   return (
@@ -15,14 +20,22 @@ const prodfile = () => {
             <h4 className="job-title">Software developer</h4>
             <SkillList />
           </div>
-          <div className="card">
+          <div className="card photo-card">
             <div className="my-photo">
               <img src={myPhoto} alt="Kai" className="my-photo" />
             </div>
             <div className="text-center sns-link">
-              <a href="https://www.linkedin.com/in/kaishu-kawano/" className="text-decoration-none text-link">Linked in</a>
-              <Link to="/AboutPage" className="text-decoration-none see-more-text">more about me</Link>
+              <a href="https://www.linkedin.com/in/kaishu-kawano/" >
+              <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a href="https://www.wantedly.com/id/kaishu" >
+              <img src={wantedly} alt="wantedly" className="wantedly m-2" />
+              </a>
+              <a href="https://github.com/kaishu-design" >
+              <FontAwesomeIcon icon={faSquareGithub} style={{color: "#ff9d2e",}} />
+              </a>
             </div>
+              <Link to="/AboutPage" className="text-center btn btn-outline-secondary text-decoration-none see-more-text">more about me</Link>
           </div>
         </div>
       </div>
